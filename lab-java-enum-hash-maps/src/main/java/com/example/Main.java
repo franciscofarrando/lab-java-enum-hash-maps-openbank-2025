83646feea47b69c85ac3482f6c852477919b636c
Francisco Ismael Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-13 19:33:31 +0200

    ejercicio terminado

--- lab-java-enum-hash-maps/src/main/java/com/example/Main.java
+++ lab-java-enum-hash-maps/src/main/java/com/example/Main.java
@@ -1,9 +1,40 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Map<String, Student> studentsHashMap = new HashMap<>();
+
+        //studentsHashMap.put("Alfonso",99);
+
+        //System.out.println(studentsHashMap.get("Alfonso"));
+
+        //Creo 4 students
+        Student student1 = new Student("Alsonso", 90);
+        Student student2 = new Student("Florencio", 70);
+        Student student3 = new Student("Mercedes", 99);
+        Student student4 = new Student("Luz", 78);
+
+        //los agrego al map
+
+        studentsHashMap.put(student1.getName(),student1);
+        studentsHashMap.put(student2.getName(),student2);
+        studentsHashMap.put(student3.getName(),student3);
+        studentsHashMap.put(student4.getName(),student4);
+
+
+        studentsHashMap.values().forEach(System.out::println);
+
+
+        System.out.println("-------------------");
+        //aumento un 10% la nota con increaseGrade
+        Map<String, Student> studentsUpdatedMap = Student.increaseGrades(studentsHashMap);
+
+        studentsHashMap.values().forEach(System.out::println);
 
     }
 }
--- lab-java-enum-hash-maps/src/main/java/com/example/Student.java
+++ lab-java-enum-hash-maps/src/main/java/com/example/Student.java
@@ -1,4 +1,53 @@
 package com.example;
 
+import java.util.Map;
+
 public class Student {
+    private String name;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+
+
+    //increaseGrade aumenta el 10% pero no pasa del los 100 puntos maximos de grade.
+
+    public static Map<String, Student> increaseGrades(Map<String,Student> students) {
+        for (Map.Entry<String,Student> entry : students.entrySet()){
+            Student student = entry.getValue();
+            int currentGrade = student.getGrade();
+            int incresedGrade = (int) Math.round(currentGrade*1.10);
+            if (incresedGrade>100){
+                incresedGrade = 100;
+            }
+            student.setGrade(incresedGrade);
+        }
+        return students;
+    }
 }
